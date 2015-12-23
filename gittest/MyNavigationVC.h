fc4ce3c7d3d71d504ad3318e6b110cdc52935e69
chenfalei <chenfalei@chenfalei-macpro.local>
2015-12-23 13:48:27 +0800

    aaaa

---
+++ gittest/MyNavigationVC.h
@@ -0,0 +1,13 @@
+//
+//  MyNavigationVC.h
+//  gittest
+//
+//  Created by chenfalei on 15/12/23.
+//  Copyright © 2015年 chenfalei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyNavigationVC : UINavigationController
+
+@end
---
+++ gittest/MyNavigationVC.m
@@ -0,0 +1,13 @@
+//
+//  MyNavigationVC.m
+//  gittest
+//
+//  Created by chenfalei on 15/12/23.
+//  Copyright © 2015年 chenfalei. All rights reserved.
+//
+
+#import "MyNavigationVC.h"
+
+@implementation MyNavigationVC
+
+@end
